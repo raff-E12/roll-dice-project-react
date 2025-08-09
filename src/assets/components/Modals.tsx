d3f12bcc41ed831fd927fe1aeeeea741814fd31d
Raff_12 <raffelecoppola381@gmail.com>
2025-08-09 18:21:40 +0200

    feat: Creazione del Prototipo..

--- src/assets/components/Modals.tsx
+++ src/assets/components/Modals.tsx
@@ -7,11 +7,24 @@ export default function Modals() {
     <section className='modal-sc'>
 
         <div className='modals'>
+
             <div id='logo-modal'>
                 <img src={LogoPage} alt="logo" />
                 <img src={BackFig} alt="fig"/>
             </div>
-            <h3>Benvenuto</h3>
+            
+            <div className='container-modal font-lexend'>
+                <h3 className='text-2xl font-extrabold'>Benvenuto Nei Gioco Dei Dati</h3>
+                <p className='font-medium text-sm'>Preparati a sfidare la sorte! Qui il dado è il vero protagonista: basta un clic per 
+                    lanciarlo e scoprire se la fortuna è dalla tua parte.
+                    Che tu voglia giocare per divertimento, sfidare un amico o scalare la classifica, 
+                    ogni lancio è un’occasione per vincere e migliorare le tue statistiche.
+                    Le regole sono semplici: scegli, lancia, e guarda il dado rotolare.
+                    ⚡ Consiglio del giorno: la fortuna premia gli audaci… ma anche chi lancia spesso!
+                    Pronto a iniziare? Premi Gioca e lascia che il destino faccia il suo corso.</p>
+            </div>
+
+            <button className='btn-modal font-montserrant'>Inizia</button>
         </div>
 
     </section>
--- src/assets/pages/MainPage.tsx
+++ src/assets/pages/MainPage.tsx
@@ -7,11 +7,25 @@ export default function MaingPage() {
   return (<>
    <main className='main-sc'>
 
-      <div className='logo-sc'>
-        <img src={MainLogo} alt="logo-sc" />
-      </div>
+     <nav className='navbar-sc'>
+        <div className='logo-sc'>
+          <img src={MainLogo} alt="logo-sc" />
+        </div>
 
-     <Modals />
+        <div className='dropmenu-sc'>
+
+           <div className='icon-drop-menu'>
+            <i className='material-symbols-outlined'>menu</i>
+           </div>
+
+            <ul className='drop-menu'>
+                <li>Statitiche</li>
+                <li>Info</li>
+                <li>Temi</li>
+            </ul>
+        </div>
+     </nav>
+
    </main>
   </>)
 }
